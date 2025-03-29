9f85dafcf00d16c675735e67c504776d51c43fc5
Nikhil <Nikhil1011@hotmail.com>
2025-03-29 15:45:05 -0500

    added active position list for ball

--- ball.py
+++ ball.py
@@ -4,12 +4,18 @@ BALL_LENGTH = 2
 class Ball:
     speed = 0
     vector = (0, 0)
-    position = (0, 0)
+    position = (0, 0) # refers to top left corner of ball
+    length = 0
+    active_pos_list = []
     
-    def __init__(self, speed, vector, position):
+    def __init__(self, speed, vector, position, length):
         self.speed = speed # need to initialize these
         self.vector = vector # passed in from app
-        self.position = position # initialize to center of screen
+        self.position = position # initialize to near center of screen
+        self.length = length
+        for i in range(length):
+            for j in range(length):
+                self.active_pos_list.append((self.position[0] + (i - 1), self.position[1] + (j - 1)))
 
     def set_speed(self, speed):
         self.speed = speed
@@ -20,6 +26,9 @@ class Ball:
     def set_position(self, position):
         self.position = position
 
+    def set_length(self, length):
+        self.length = length
+
     def get_speed(self):
         return self.speed
     
@@ -28,5 +37,8 @@ class Ball:
     
     def get_position(self):
         return self.position
+
+    def get_length(self):
+        return self.length
     
 
