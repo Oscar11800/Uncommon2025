9a415d96d6b95bcca70295fb84210792f6486914
Nikhil <Nikhil1011@hotmail.com>
2025-03-29 14:27:57 -0500

    edited app.py for additional testing

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pyxel
 
 # Game Variables (Currently dummy values)
 BALL_SPEED = 1
-BALL_RADIUS = 5
+BALL_LENGTH = 2
 WINNING_HEIGHT = 100
 
 
@@ -13,13 +13,13 @@ WINNING_HEIGHT = 100
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
+        pyxel.init(192, 108)
         self.x = 0
         pyxel.run(self.update, self.draw)
     def update(self):
         self.x = (self.x + 1) % pyxel.width
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+        pyxel.rect(self.x, 0, 2, 2, 9)
 
 App()
