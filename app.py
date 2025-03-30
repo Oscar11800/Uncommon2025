680e7b24a5e957f907981124d5f2f28c0c3ae766
GreenbackCafe <pietro@stabile.org>
2025-03-29 21:01:25 -0500

    fixed variable reference and null dereference issues

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class App:
         # avoid delays on first sound play
         #threading.Thread(target=playsound, args=('assets\\silent_quarter-second.wav',), daemon=True).start()
 
-        threading.Thread(target=App.music, args=(os.path.dirname(__file__) + '\\assets\\background_music_first_half.mp3',), daemon=True).start()
+        self.music = threading.Thread(target=App.music, args=(os.path.dirname(__file__) + '\\assets\\background_music_first_half.mp3',), daemon=True).start()
 
         # Config game window
         self.w = 192
@@ -154,8 +154,10 @@ class App:
     def fix_missing_live_blocks(self): # checks if either grid is missing a live block and respawns one if so
         if not self.grid_left.has_live():
           self.grid_left.spawn_block(self.player1_block_index)
+          self.player1_block_index += 1
         if not self.grid_right.has_live():
           self.grid_right.spawn_block(self.player2_block_index)
+          self.player2_block_index += 1
 
     def update(self):
         if(self.curr_frame == 0 and pyxel.btn(pyxel.KEY_Z)):
@@ -178,7 +180,6 @@ class App:
           self.make_square(2, 0, 2)
           self.game_ball.set_vector([1, 0])
           #threading.Thread(target=playsound, args=('assets\\make_invincible_line.wav',), daemon=True).start()
-        self.x = self.x + 1
           
         if(self.game_running):
             self.curr_frame = self.curr_frame + 1
@@ -190,6 +191,11 @@ class App:
             self.check_collisions()
             # update ball
             self.game_ball.set_position((self.game_ball.position[0] + self.game_ball.vector[0], self.game_ball.position[1] + self.game_ball.vector[1]))
+        self.fix_missing_live_blocks()
+
+        if self.player1_block_index >= 1 or self.player2_block_index >= 1:
+          self.music.join()
+          self.music = threading.Thread(target=App.music, args=(os.path.dirname(__file__) + '\\assets\\background_music_second_half.mp3',), daemon=True).start()
         
     def draw(self):
         pyxel.cls(0)
