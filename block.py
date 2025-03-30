c8a1104b5f56cd16792cfbbbda8caf58fce5ff44
Oscar Chen <oscar1149412@gmail.com>
2025-03-30 01:32:38 -0500

    up to grid thing

--- block.py
+++ block.py
@@ -31,109 +31,6 @@ class Block:
         self.grid = grid
         self.originX = x
         self.originY = y
-        for current_square in self.squares:
-            #self.grid.grid[(int)(self.squares[i-1].get_x())][(int)(self.squares[i-1].get_y())] = self.squares[i-1]
-            current_square.print_square()
-            print(f"TYPEEEEEEEEE: {self.type}")
-            self.grid.print_grid()
-            self.grid.add_square(current_square, (int)(current_square.get_x()), (int)(current_square.get_y()))
-
-    def _rotate_helper(self, x, y, s):
-        x -= self.originX
-        y -= self.originY
-
-        x_new = -y * s
-        y_new = x * s
-
-        x = x_new + self.originX
-        y = y_new + self.originY
-
-        return (x, y)
-
-    def rotate(self, direction):
-        if not self.is_live:
-            return
-
-        # call update for all four squares
-        (x0, y0) = (self.squares[0].getX(), self.squares[0].getY())
-        (x1, y1) = (self.squares[1].getX(), self.squares[1].getY())
-        (x2, y2) = (self.squares[2].getX(), self.squares[2].getY())
-        (x3, y3) = (self.squares[3].getX(), self.squares[3].getY())
-        
-        # s is sine of the angle (cosine is 0 either way)
-        if (direction == Direction.LEFT):
-            s = 1
-        elif (direction == Direction.RIGHT):
-            s = -1
-                
-        self.squares[0].update(Block._rotate_helper(self, x0, y0, s))
-        self.squares[1].update(Block._rotate_helper(self, x1, y1, s))
-        self.squares[2].update(Block._rotate_helper(self, x2, y2, s))
-        self.squares[3].update(Block._rotate_helper(self, x3, y3, s))
-
-        if (direction == Direction.RIGHT): self.rotation_state += 1
-        elif (direction == Direction.LEFT): self.rotation_state -= 1
-        self.rotation_state %= 4
-
-    def set_speed(new_speed):
-        Block.blocks_speed = new_speed
-    
-    def move_down(self):
-        self.originY -= 1
-        for i in range(len(self.squares)):
-            prev_y = self.squares[i].get_y()
-            self.squares[i].update(self.squares[i].get_x(), prev_y - 1)
-            if (self.grid.get_grid()[int(prev_y - 1)][int(self.squares[i].get_x())]!= None):
-                self.is_live = 0
-        self.draw()
-
-    def draw(self):
-        for sq in self.squares:
-            sq.draw(self.grid.get_grid())
-
-    def destroy(self):
-        for sq in self.squares:
-            sq.destroy()
-
-    def slide(self, direction):
-        if not self.is_live:
-            return
-        
-        for sq in self.squares:
-            if sq.getX() == 0 and direction == Direction.LEFT:
-                return
-            if sq.getX() == self.grid.width - 1 and direction == Direction.RIGHT:
-                return
-            
-        for sq in self.squares:
-            if direction == Direction.LEFT:
-                sq.update(sq.getX() - 1, sq.getY())
-            elif direction == Direction.RIGHT:
-                sq.update(sq.getX() + 1, sq.getY())
-
-    def update(self):
-        
-        if not self.is_live:
-            return
-
-        if pyxel.btn(pyxel.KEY_Q) and self.player == 0:
-            self.rotate(Direction.LEFT)
-        if pyxel.btn(pyxel.KEY_E) and self.player == 0:
-            self.rotate(Direction.RIGHT)
-        if pyxel.btn(pyxel.KEY_U) and self.player == 1:
-            self.rotate(Direction.LEFT)
-        if pyxel.btn(pyxel.KEY_O) and self.player == 1:
-            self.rotate(Direction.RIGHT)
-        
-        if pyxel.btn(pyxel.KEY_A) and self.player == 0:
-            self.slide(Direction.LEFT)
-        if pyxel.btn(pyxel.KEY_D) and self.player == 0:
-            self.slide(Direction.RIGHT)
-        if pyxel.btn(pyxel.KEY_J) and self.player == 1:
-            self.slide(Direction.LEFT)
-        if pyxel.btn(pyxel.KEY_L) and self.player == 1:
-            self.slide(Direction.RIGHT)
-        
         match type:
             case BlockType.O:
                 self.squares[1].update(self.x + 1, self.y)
@@ -225,7 +122,6 @@ class Block:
                                               (self.grid.square_size * self.squares[3].get_y()))
                 self.originX = self.x + 1
                 self.originY = self.y - 1
-                print("ASHASHR")
             case BlockType.T:
                 self.squares[1].update(self.x + 1, self.y)
                 self.squares[1].update_pixels(self.grid.x_pix_offset_left + (self.grid.square_size * self.squares[1].get_x()),
@@ -241,5 +137,111 @@ class Block:
                                               (self.grid.square_size * self.squares[3].get_y()))
                 self.originX = self.x + 1
                 self.originY = self.y
+        
+        for current_square in self.squares:
+            #self.grid.grid[(int)(self.squares[i-1].get_x())][(int)(self.squares[i-1].get_y())] = self.squares[i-1]
+            current_square.print_square()
+            print(f"TYPEEEEEEEEE: {self.type}")
+            self.grid.print_grid()
+            self.grid.add_square(current_square, (int)(current_square.get_x()), (int)(current_square.get_y()))
+
+    def _rotate_helper(self, x, y, s):
+        x -= self.originX
+        y -= self.originY
+
+        x_new = -y * s
+        y_new = x * s
+
+        x = x_new + self.originX
+        y = y_new + self.originY
+
+        return (x, y)
+
+    def rotate(self, direction):
+        if not self.is_live:
+            return
+
+        # call update for all four squares
+        (x0, y0) = (self.squares[0].getX(), self.squares[0].getY())
+        (x1, y1) = (self.squares[1].getX(), self.squares[1].getY())
+        (x2, y2) = (self.squares[2].getX(), self.squares[2].getY())
+        (x3, y3) = (self.squares[3].getX(), self.squares[3].getY())
+        
+        # s is sine of the angle (cosine is 0 either way)
+        if (direction == Direction.LEFT):
+            s = 1
+        elif (direction == Direction.RIGHT):
+            s = -1
+                
+        self.squares[0].update(Block._rotate_helper(self, x0, y0, s))
+        self.squares[1].update(Block._rotate_helper(self, x1, y1, s))
+        self.squares[2].update(Block._rotate_helper(self, x2, y2, s))
+        self.squares[3].update(Block._rotate_helper(self, x3, y3, s))
+
+        if (direction == Direction.RIGHT): self.rotation_state += 1
+        elif (direction == Direction.LEFT): self.rotation_state -= 1
+        self.rotation_state %= 4
+
+    def set_speed(new_speed):
+        Block.blocks_speed = new_speed
+    
+    def move_down(self):
+        self.originY -= 1
+        for i in range(len(self.squares)):
+            prev_y = self.squares[i].get_y()
+            self.squares[i].update(self.squares[i].get_x(), prev_y - 1)
+            
+            self.squares[i].update_pixels(self.grid.x_pix_offset_left, self.grid.pix_height - self.grid.y_pix_offset_bot - (self.grid.square_size * (self.squares[i].get_y()+1)))
+            
+            if (self.grid.get_grid()[int(self.squares[i].get_x())][int(prev_y - 1)]!= None):
+                self.is_live = 0
+        self.draw()
+
+    def draw(self):
+        for sq in self.squares:
+            sq.draw(self.grid.get_grid())
+
+    def destroy(self):
+        for sq in self.squares:
+            sq.destroy()
+
+    def slide(self, direction):
+        if not self.is_live:
+            return
+        
+        for sq in self.squares:
+            if sq.getX() == 0 and direction == Direction.LEFT:
+                return
+            if sq.getX() == self.grid.width - 1 and direction == Direction.RIGHT:
+                return
+            
+        for sq in self.squares:
+            if direction == Direction.LEFT:
+                sq.update(sq.getX() - 1, sq.getY())
+            elif direction == Direction.RIGHT:
+                sq.update(sq.getX() + 1, sq.getY())
+
+    def update(self):
+        
+        if not self.is_live:
+            return
+
+        if pyxel.btn(pyxel.KEY_Q) and self.player == 0:
+            self.rotate(Direction.LEFT)
+        if pyxel.btn(pyxel.KEY_E) and self.player == 0:
+            self.rotate(Direction.RIGHT)
+        if pyxel.btn(pyxel.KEY_U) and self.player == 1:
+            self.rotate(Direction.LEFT)
+        if pyxel.btn(pyxel.KEY_O) and self.player == 1:
+            self.rotate(Direction.RIGHT)
+        
+        if pyxel.btn(pyxel.KEY_A) and self.player == 0:
+            self.slide(Direction.LEFT)
+        if pyxel.btn(pyxel.KEY_D) and self.player == 0:
+            self.slide(Direction.RIGHT)
+        if pyxel.btn(pyxel.KEY_J) and self.player == 1:
+            self.slide(Direction.LEFT)
+        if pyxel.btn(pyxel.KEY_L) and self.player == 1:
+            self.slide(Direction.RIGHT)
         
         self.draw()
